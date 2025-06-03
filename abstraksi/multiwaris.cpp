2b4baa9da8417f93c0f2a67856c702bd1e9912a5
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-03 18:31:33 +0700

    pembuatan class budi

--- abstraksi/multiwaris.cpp
+++ abstraksi/multiwaris.cpp
@@ -30,3 +30,14 @@ class pelajar : virtual public orang {
     }
 };
 
+class budi : public pekerja, public pelajar {
+    public :
+    budi (int pUmur) :
+    pekerja(pUmur),
+    pelajar(pUmur),
+    orang(pUmur)// hal ini dapat dilakukan jika menggunakan virtual
+    {
+        cout << "Budi dibuat\n" << endl;
+    }
+};
+

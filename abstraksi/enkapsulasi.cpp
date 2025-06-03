d2006dd69051b8be99895523d77df54b619bc7eb
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-03 18:17:52 +0700

    selesai

--- abstraksi/enkapsulasi.cpp
+++ abstraksi/enkapsulasi.cpp
@@ -8,8 +8,23 @@ class remoteLampu {
     void setSaklarNo(int i, string value) {
         saklarNo[i] = value;
     }
-    string getSaklar (int i) {
+    string getSaklarNo(int i) {
         return saklarNo[i];
     }
 };
 
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+}

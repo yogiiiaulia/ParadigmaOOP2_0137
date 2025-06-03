3050c3a6289350dcf25de3524862b7890517d02f
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-03 18:13:52 +0700

    pembuatan class remote lampu

--- abstraksi/enkapsulasi.cpp
+++ abstraksi/enkapsulasi.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class remoteLampu {
+    private :
+    string saklarNo[10];
+    public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklar (int i) {
+        return saklarNo[i];
+    }
+};
+

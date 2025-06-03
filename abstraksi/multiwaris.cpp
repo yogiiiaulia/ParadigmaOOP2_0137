a22231061c690d4a60e7936b31ad070bc5682a41
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-03 18:31:13 +0700

    pembuatan class pelajar

--- abstraksi/multiwaris.cpp
+++ abstraksi/multiwaris.cpp
@@ -20,3 +20,13 @@ class pekerja : virtual public orang{
         cout << "pekerja dibuat\n" << endl;
     }
 };
+
+class pelajar : virtual public orang {
+    public:
+    pelajar (int pUmur) :
+    orang (pUmur)
+    {
+        cout << "pelajar dibuat" << endl;
+    }
+};
+

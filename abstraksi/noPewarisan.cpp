bfa3c49767d68f97cf5adbe16a2f86b6b602be44
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-06-03 18:50:51 +0700

    selesai

--- abstraksi/noPewarisan.cpp
+++ abstraksi/noPewarisan.cpp
@@ -1,3 +1,32 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final{
+    public :
+    virtual void perkenalan(){
+        cout << "Hallo saya Function dari base class";
+
+    }
+};
+
+class baseClass1 final{
+    public :
+    virtual void perkenalan(){
+        cout << "Hallo saya Function dari base class";
+
+    }
+};
+
+class derivedClass : public baseClass1 {
+    public :
+    void perkenalan (){
+        cout << "Hallo saya Function dari derived class"
+    }
+};
+
+int main(){
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
